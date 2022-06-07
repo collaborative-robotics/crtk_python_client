1f1782e6bae91f4b81ba683fd3a119a38822e9e3
Anton Deguet <anton.deguet@jhu.edu>
2022-06-07 17:54:44 -0400

    added simple class with measured_cp only

--- src/crtk/__init__.py
+++ src/crtk/__init__.py
@@ -4,10 +4,13 @@
 # Copyright (c) 2016-2021 Johns Hopkins University, University of Washington, Worcester Polytechnic Institute
 # Released under MIT License
 
-__all__ = ["wait_move_handle", "utils"]
+__all__ = ["wait_move_handle", "utils", "measured_cp"]
 
 # handle classes
 from .wait_move_handle import wait_move_handle
 
 # utilities
 from .utils import utils
+
+# example classes
+from .measured_cp import measured_cp
