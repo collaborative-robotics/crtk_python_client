a8081fdf832b5a19e434b6d3f9d2e5657f9dc0a7
Brendan Burkhart <brendan.f.burkhart@gmail.com>
2023-06-30 12:56:20 -0400

    Use crtk.joystick_button to use ral

--- src/crtk/joystick_button.py
+++ src/crtk/joystick_button.py
@@ -5,42 +5,36 @@
 # Released under MIT License
 
 import threading
-import rospy
 import sensor_msgs.msg
 
 class joystick_button:
-    def __init__(self, button_name, index = 0):
-        self.__button_name = button_name
-        self.__index = index
-        self.__value = None
-        self.__user_callback = None
-        self.__event = threading.Event()
-        self.__subscriber = rospy.Subscriber(button_name,
-                                             sensor_msgs.msg.Joy, self.__callback,
-                                             queue_size = 10)
+    def __init__(self, ral, button_name, index = 0):
+        self._index = index
+        self._value = None
+        self._user_callback = None
+        self._event = threading.Event()
+        self._subscriber = ral.subscriber(button_name, sensor_msgs.msg.Joy,
+                                          self._callback, queue_size = 10)
 
-    def __del__(self):
-        self.__subscriber.unregister()
-
-    def __callback(self, event):
-        self.__value = event.buttons[self.__index]
-        self.__event.set()
-        if not self.__user_callback is None:
-            self.__user_callback(self.__value)
+    def _callback(self, event):
+        self._value = event.buttons[self._index]
+        self._event.set()
+        if not self._user_callback is None:
+            self._user_callback(self._value)
 
     def set_callback(self, callback = None):
-        self.__user_callback = callback
+        self._user_callback = callback
 
     def value(self):
-        return self.__value
+        return self._value
 
     def wait(self, timeout = None, value = None):
         ready = False
         while not ready:
-            self.__event.clear()
-            if not self.__event.wait(timeout):
+            self._event.clear()
+            if not self._event.wait(timeout):
                 return False
             else:
-                if value == None or value == self.__value:
+                if value == None or value == self._value:
                     ready = True
         return False
