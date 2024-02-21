a15c0853a1caa4d8fc74c22d2940b632213bac65
Anton Deguet <anton.deguet@jhu.edu>
2024-02-21 14:55:17 -0500

    Merged in ROS2 version so the code works with ROS1 and ROS2

--- src/crtk/__init__.py
+++ src/crtk/__init__.py
@@ -4,10 +4,17 @@
 # Copyright (c) 2016-2023 Johns Hopkins University, University of Washington, Worcester Polytechnic Institute
 # Released under MIT License
 
-__all__ = ["ral", "wait_move_handle", "utils", "joystick_button", "measured_cp"]
+__all__ = ['wait_move_handle', 'utils', 'joystick_button', 'measured_cp']
 
 # ros abstraction layer
-from .ral import ral
+import os
+__ros_version_string = os.environ['ROS_VERSION']
+if __ros_version_string == '1':
+    __all__.append('ros_ral1')
+    from .ral_ros1 import ral
+elif __ros_version_string == '2':
+    __all__.append('ros_ral2')
+    from .ral_ros2 import ral
 
 # handle classes
 from .wait_move_handle import wait_move_handle
